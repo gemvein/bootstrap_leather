2971f89bce0baf98e765dede0652b49f9eae694a
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-05-15 09:16:02 -0600

    Added badge_to method

--- app/helpers/bootstrap_leather_helper.rb
+++ app/helpers/bootstrap_leather_helper.rb
@@ -126,7 +126,12 @@ module BootstrapLeatherHelper
     html.html_safe
   end
 
-  def badge(content, type)
+  def badge_to(text, value, link, html_options={})
+    html_options[:href] = url_for link
+    render(:partial => 'bootstrap_leather/badge_to', :locals => {:text => text, :value => value, :html_options => html_options})
+  end
+
+  def badge(content, type = nil)
     render(:partial => 'bootstrap_leather/badge', :locals => {:content => content, :type => type})
   end
 
