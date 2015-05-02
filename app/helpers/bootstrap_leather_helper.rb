a8f53987c390c1155442a70c549eae0439cb2199
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-05-02 11:38:29 -0600

    Fixed alert-error to be alert-danger class

--- app/helpers/bootstrap_leather_helper.rb
+++ app/helpers/bootstrap_leather_helper.rb
@@ -56,7 +56,7 @@ module BootstrapLeatherHelper
   def flash_class(level)
     case level
       when :notice then 'alert-info'
-      when :error then 'alert-error'
+      when :error then 'alert-danger'
       when :alert then 'alert-warning'
     end
   end
