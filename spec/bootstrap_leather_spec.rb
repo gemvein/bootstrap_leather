fab24704820839eb1abfd176d095259636d88954
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-05-16 14:23:21 -0600

    Removed rspec fully

--- spec/bootstrap_leather_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe BootstrapLeather do
-  it 'should return correct version string' do
-    BootstrapLeather.version_string.should == "BootstrapLeather version #{BootstrapLeather::VERSION}"
-  end
-end
--- spec/dummy/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def index
-    flash[:alert] = 'This is an alert message.'
-    flash[:error] = 'This is an error message.'
-    flash[:notice] = 'This is a notice message.'
-  end
-
-end
