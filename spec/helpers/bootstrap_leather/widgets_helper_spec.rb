02c2f21c32bfa96767a604d1ecb1a1be4104756e
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-04-07 13:51:05 -0600

    Widgets helper specs

---
+++ spec/helpers/bootstrap_leather/widgets_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+module BootstrapLeather
+  RSpec.describe WidgetsHelper, folder: :helpers do
+    helper GridHelper
+    describe '#add_widget and #render_widgets' do
+      before do
+        helper.add_widget do
+          Faker::Lorem.paragraph
+        end
+      end
+      subject { helper.render_widgets('md', 3) }
+      it 'shows the tab links' do
+        expect(subject).to(
+          have_tag(:div, with: { class: 'col-md-3' }) do
+            with_tag :div, with: { class: 'well' }
+          end
+        )
+      end
+    end
+  end
+end
