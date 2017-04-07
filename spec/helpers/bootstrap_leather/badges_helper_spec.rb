683c5b7d4253dad8f0f9d39342cb9396c3a67c47
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-04-07 10:07:41 -0600

    Badges helper specs

---
+++ spec/helpers/bootstrap_leather/badges_helper_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+module BootstrapLeather
+  RSpec.describe BadgesHelper, folder: :helpers do
+    describe '#badge' do
+      describe 'with nil' do
+        subject { helper.badge nil }
+        it { should eq '' }
+      end
+      describe 'with a number' do
+        subject { helper.badge 1 }
+        it { should have_tag :span, class: 'badge', text: /1/ }
+      end
+      describe 'with a string' do
+        subject { helper.badge '1' }
+        it { should have_tag :span, class: 'badge', text: /1/ }
+      end
+      describe 'with a type' do
+        subject { helper.badge '1', 'success' }
+        it { should have_tag :span, class: 'badge-success', text: /1/ }
+      end
+    end
+
+    describe '#badge_to' do
+      subject do
+        helper.badge_to 'Link Text', 99, 'http://example.com', class: 'example'
+      end
+      it do
+        should have_tag(
+                 :a,
+                 class: 'example',
+                 href: 'http://example.com',
+                 text: /Link Text/
+               )
+      end
+      it do
+        should have_tag(:span, class: 'badge', text: /99/)
+      end
+    end
+  end
+end
