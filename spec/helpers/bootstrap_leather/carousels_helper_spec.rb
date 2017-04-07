238538896536d6451efdb12d1147c3f8db12e070
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-04-07 11:28:09 -0600

    More Carousels helper specs

--- spec/helpers/bootstrap_leather/carousels_helper_spec.rb
+++ spec/helpers/bootstrap_leather/carousels_helper_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 module BootstrapLeather
   RSpec.describe CarouselsHelper, folder: :helpers do
     helper IconsHelper
+    helper HeadHelper
     helper FootHelper
 
     describe '#carousel' do
       subject do
-        helper.carousel 'css-id-for-carousel', DooDad.all do |item|
+        helper.carousel 'css-id', DooDad.all do |item|
           %(
 <div class='carousel-caption'><h4>#{item.name}</h4></div>
 <p>#{item.body}</p>
@@ -16,13 +17,28 @@ module BootstrapLeather
       end
       it do
         should have_tag :div, with: {
-          class: 'carousel', id: 'css-id-for-carousel'
+          class: 'carousel', id: 'css-id'
         }
       end
       it { should have_tag :div, with: { class: 'item' }, count: 5}
     end
 
     describe '#carousel_with_thumbnails' do
+      subject do
+        helper.carousel_with_thumbnails 'css-id', DooDad.all do |item|
+          %(
+<div class='carousel-caption'><h4>#{item.name}</h4></div>
+<p>#{item.body}</p>
+#{image_tag 'src.png', data: { thumbnail: 'thumb.png' }}
+).html_safe
+        end
+      end
+      it do
+        should have_tag :div, with: {
+          class: 'carousel-with-thumbnails', id: 'css-id'
+        }
+      end
+      it { should have_tag :div, with: { class: 'item' }, count: 5}
     end
   end
 end
